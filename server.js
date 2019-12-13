620bc24f9c35ea9ecd38af63ea293dd382ba4356
Brendan Berry <53982094+Pickingberries@users.noreply.github.com>
2019-12-12 19:47:24 -0800

    Posting notes now works

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var noteObj = [
+
+]
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -20,19 +24,11 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", function (err, data) {
-        if (err) throw err;
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(data);
-    });
+    return res.json(noteObj);
 });
 app.post("/api/notes", function (req, res) {
-    var note = res.body
-    fs.readFile(__dirname + "/db/db.json", function (err, data) {
-        if (err) throw err;
-        JSON.parse(data).db.push(note)
-    });
+    var note = req.body;
+    noteObj.push(note);
     res.json(note);
 });
 app.post("/api/notes/:id", function (req, res) {
