8fd336fad72bb79bc4a3ae2b7234282302cab093
Brendan Berry <53982094+Pickingberries@users.noreply.github.com>
2019-12-12 21:16:32 -0800

    Everything works aside from note deletion

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var noteObj = [
-
-]
+//I'm using an object instead of the database file because I couldn't get it to function as smoothly
+//I'd like to figure out how to use the file successfully because I have a feeling it will make it easier to delete objects
+var noteObj = []
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -31,8 +31,23 @@ app.post("/api/notes", function (req, res) {
     noteObj.push(note);
     res.json(note);
 });
-app.post("/api/notes/:id", function (req, res) {
 
+app.get("/api/notes/:id", function (req, res) {
+
+    var request = parseInt(req.params.id)
+    console.log(request)
+    console.log(noteObj.id)
+    //noteObj.id isn't returning anything for some reason
+    //I'm still trying to figure out why
+    if (noteObj.id === request) {
+        var index = noteObj.indexOf(request);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+        return res.json(noteObj)
+    }
+    else 
+        res.send("Please enter the ID of the note you would like to delete.")
 });
 
 
